d1a8ca25e25cd323b09dad02ae84585040e05e3a
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-07-30 14:26:54 +0700

    updated domain folder with Employee and User models

--- src/main/java/com/manage/employee/myproject/domain/Employee.java
+++ src/main/java/com/manage/employee/myproject/domain/Employee.java
@@ -1,5 +1,6 @@
 package com.manage.employee.myproject.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,22 +20,25 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class Employee {
-
   @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
-  private String name;
-  private String gender;
-  private String email;
-  private String date;
-  private long age;
 
-  public Employee(String name, String gender,
-  String email, String date, long age){
-    this.name = name;
-    this.gender = gender;
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "email")
+  private String email;
+
+  public Employee(String firstName, String lastName,
+  String email){
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.email = email;
-    this.date = date;
-    this.age = age;
   }
+
+
 }
---
+++ src/main/java/com/manage/employee/myproject/domain/User.java
@@ -0,0 +1,29 @@
+package com.manage.employee.myproject.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name="first_name")
+  private String firstName;
+
+  @Column(name="last_name")
+  private String lastName;
+
+  @Column(name="email")
+  private String email;
+
+  @Column(name="password")
+  private String password;
+}
