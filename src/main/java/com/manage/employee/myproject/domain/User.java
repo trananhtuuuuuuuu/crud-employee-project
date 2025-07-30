09e1753bc3bd7c0cb7199766aef433c427b5c59b
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-07-30 16:01:34 +0700

    updated domain

--- src/main/java/com/manage/employee/myproject/domain/User.java
+++ src/main/java/com/manage/employee/myproject/domain/User.java
@@ -1,11 +1,18 @@
 package com.manage.employee.myproject.domain;
 
 
+import java.util.Collection;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -30,6 +37,16 @@ public class User {
 
   @Column(name="password")
   private String password;
+
+
+  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JoinTable(
+    name = "users_roles",
+    joinColumns = @JoinColumn(
+    name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(
+    name = "role_id", referencedColumnName = "id"))
+  private Collection<Role> roles;
 
   public User(){
 
