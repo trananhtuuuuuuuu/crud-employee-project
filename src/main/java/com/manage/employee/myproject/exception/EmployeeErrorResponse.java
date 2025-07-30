2321ce303c5cb744dddf415a7b343634d1f55ac7
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-07-30 09:27:12 +0700

    create exception

---
+++ src/main/java/com/manage/employee/myproject/exception/EmployeeErrorResponse.java
@@ -0,0 +1,17 @@
+package com.manage.employee.myproject.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeErrorResponse {
+  private int status;
+  private String message;
+  private long timeStamp;
+
+}
---
+++ src/main/java/com/manage/employee/myproject/exception/EmployeeNotFoundException.java
@@ -0,0 +1,15 @@
+package com.manage.employee.myproject.exception;
+
+public class EmployeeNotFoundException extends RuntimeException{
+  public EmployeeNotFoundException(String message){
+    super(message);
+  }
+
+  public EmployeeNotFoundException(String message, Throwable cause){
+    super(message, cause);
+  }
+
+  public EmployeeNotFoundException(Throwable cause){
+    super(cause);
+  }
+}
